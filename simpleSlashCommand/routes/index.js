cd82f68d6e3cc3aa891472b09e96a610780d9ff5
Georgina Cáceres <georgimeli@gmail.com>
2018-06-24 22:21:09 -0300

    simpleSlashCommand that show random images

--- simpleSlashCommand/routes/index.js
+++ simpleSlashCommand/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* GET slash command. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    // Create an array of strings with image url's
+    let images = [
+        "https://memegenerator.net/img/instances/28654190/me-neither.jpg",
+        "https://img.memecdn.com/me-neither_o_830020.jpg",
+        "https://meme.xyz/uploads/posts/t/l-25906-i-have-no-idea-what-im-doing.jpg",
+        "http://i0.kym-cdn.com/photos/images/newsfeed/000/107/973/I-smile-because-i-have-no-idea-whats-going-on.jpg"
+    ];
+    // Select a random image to show
+    imageIndex = Math.floor(Math.random() * images.length);
+
+    // IMPORTANT!
+    let data = {
+        response_type: 'in_channel', // public to the channel. If use 'ephemeral' only will be visible to the user.
+        text: 'Aquí tienes!', // Optional text
+        attachments:[{
+            image_url: images[imageIndex]
+        }]
+    };
+    res.json(data);
 });
 
 module.exports = router;
